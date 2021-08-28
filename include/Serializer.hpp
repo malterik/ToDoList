b03906293f6fd3a22cfff95a007f898d1b9f0104
Erik Schroeder <schroedererik@mailbox.org>
2021-08-28 18:06:12 +0200

    Clang format

--- include/Serializer.hpp
+++ include/Serializer.hpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 
-class Serializer
-{
-public:
-   virtual string toJson() = 0;
-   virtual void writeToFile(const string& files_path) = 0;
+class Serializer {
+ public:
+  virtual string toJson() = 0;
+  virtual void writeToFile(const string& files_path) = 0;
 
-protected:
-   string json_content;
+ protected:
+  string json_content;
 };
--- include/Task.hpp
+++ include/Task.hpp
@@ -1,21 +1,21 @@
-#include "Serializer.hpp"
 #include <iostream>
+
+#include "Serializer.hpp"
 using namespace std;
 
-class Task : Serializer
-{
-public:
-   Task(string name, int priority);
-   ~Task();
+class Task : Serializer {
+ public:
+  Task(string name, int priority);
+  ~Task();
 
-   string get_name();
-   int get_priority();
+  string get_name();
+  int get_priority();
 
-   string toJson();
-   void writeToFile(const string& files_path);
+  string toJson();
+  void writeToFile(const string& files_path);
 
-private:
-   string name_;
-   int priority_;
+ private:
+  string name_;
+  int priority_;
 };
 
